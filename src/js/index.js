9ac4f6079c1de394cebcfe9dff60c271b60e6d2b
Mateusz Kończal <mateuszkonczal96@gmail.com>
2020-07-28 02:10:27 +0200

    added moment.js library

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import "../scss/main.scss";
 
+import moment from "moment";
+
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
 // registerSW();
@@ -355,3 +357,12 @@ hamburger.addEventListener("click", () => {
   const nav = document.querySelector(".navigation--js");
   nav.classList.toggle("navigation--open");
 });
+
+// BIBLIOTEKA MOMENT (DATA I CZAS)
+// console.log(moment().startOf("day").fromNow());
+
+const startOfDay = moment().startOf("day").fromNow();
+console.log(startOfDay);
+
+const timePlaceholder = document.querySelector(".time--js");
+timePlaceholder.innerHTML = startOfDay;
